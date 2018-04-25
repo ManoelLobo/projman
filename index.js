8578a54b320193e715d61a276ddb1cc9404d7c66
Manoel Lobo <manoel.lobo@gmail.com>
2018-04-25 08:16:22 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve('app', 'public')));
+
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(3000);
