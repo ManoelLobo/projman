5b186b5f8d0920925c40000d66c9adfe42984be3
Manoel Lobo <manoel.lobo@gmail.com>
2018-04-27 09:57:19 -0300

    Projects listing + layout

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,5 +1,13 @@
+const { Project } = require('../models');
+
 module.exports = {
-  index(req, res) {
-    return res.render('dashboard/index');
+  async index(req, res, next) {
+    try {
+      const { user } = req.session;
+      const projects = await Project.findAll({ where: { UserId: user.id } });
+      return res.render('dashboard/index', { user, projects });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
